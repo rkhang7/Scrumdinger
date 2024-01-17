efa345fa515266832982fa5dc0c360614e8fbd31
khang <khang@tungvts-iMac-191.local>
2024-01-17 09:19:27 +0700

    create project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Khang on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
