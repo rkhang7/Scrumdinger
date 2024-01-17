9b0c812f2050ef892cd18266c3ed6cb296b01a09
khang <khang@tungvts-iMac-191.local>
2024-01-17 10:46:08 +0700

    create a color theme

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -24,6 +24,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle().strokeBorder(lineWidth: 24)
             HStack{
                 Text("Speaker 1 of 3")
