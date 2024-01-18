56b505a01226f352ef827e57a34504e806f31825
khang <khang@tungvts-iMac-191.local>
2024-01-18 13:57:34 +0700

    create the detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Khang on 18/01/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
