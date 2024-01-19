8365d0f16b8171833c86af749739092f72210313
khang <khang@tungvts-iMac-191.local>
2024-01-19 14:34:28 +0700

    pass a binding into the list view

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
+    @State private var edittingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
     var body: some View {
         List {
@@ -45,16 +46,23 @@ struct DetailView: View {
         .toolbar(){
             Button("Edit"){
                 isPresentingEditView = true
+                edittingScrum = scrum
             }
         }
         .sheet(isPresented: $isPresentingEditView){
             NavigationStack{
-                DetailEditView()
+                DetailEditView(scrum: $edittingScrum)
                     .navigationTitle(scrum.title)
                     .toolbar(){
                         ToolbarItem(placement: .cancellationAction){
                             Button("Cancel"){
                                 isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction){
+                            Button("Done"){
+                                isPresentingEditView = false
+                                scrum = edittingScrum
                             }
                         }
                     }
@@ -69,7 +77,7 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
