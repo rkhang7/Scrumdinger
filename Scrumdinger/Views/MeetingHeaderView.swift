61d8010c909c113a0d52c338df78555cbf5e2332
khang <khang@tungvts-iMac-191.local>
2024-01-24 10:13:51 +0700

    Add a state object for a soucre of truth

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MeetingHeaderView: View {
     let secondElapsed: Int
     let secondsRemaining: Int
+    let theme: Theme
     
     private var totalSeconds: Int {
         secondElapsed + secondsRemaining
@@ -28,6 +29,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -39,18 +41,20 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondElapsed: 60, secondsRemaining: 180)
+        MeetingHeaderView(secondElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
             .previewLayout(.sizeThatFits)
     }
 }
